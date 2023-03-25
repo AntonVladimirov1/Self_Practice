7d04d675594e95c643f5f4c1a6efa14934ebba15
Anton Vladimirov <l.a.v7232@gmail.com>
2023-03-24 19:38:34 -0500

    All my stuff

---
+++ day20_Array_forEach/Sample.java
@@ -0,0 +1,16 @@
+package day20_Array_forEach;
+
+public class Sample {   // Haha ....Good advise!!!
+    public static void main(String[] args) {
+
+        String[] dailyQ = {"wish you","an awesome week","Please","give up","you can","never","succeed","keep studying","everyday","no way","you will succeed!"};
+        String javaFun = "true";
+        boolean False = javaFun.length() >= 4 ? true : false;
+        while (False) {
+            System.out.println(dailyQ[2] + " " + dailyQ[5] + " " + dailyQ[3] + " " + dailyQ[7] + " " + dailyQ[8] + " " + dailyQ[10] + "\n");
+            if (False == true)
+                System.out.println("See you next week");
+
+        }
+    }
+}
