3629fb524144739978bd5bf84c6b6d7e8a74eaf2
Anton Vladimirov <l.a.v7232@gmail.com>
2023-03-26 13:34:49 -0500

    All my stuff

---
+++ utilities/UniqueElement_Array.java
@@ -0,0 +1,21 @@
+package utilities;
+
+public class UniqueElement_Array {
+    public static void main(String[] args) {
+
+        int[] array ={1,2,3,4,1,2,3,5,5,5,7};
+
+        for (int each : array) {
+            int count = 0;
+            for (int element : array) {
+                if (element == each){
+                    count++;
+                }
+            }
+            if (count ==1){
+                System.out.println("Unique element = "+each);
+            }
+        }
+
+    }
+}
