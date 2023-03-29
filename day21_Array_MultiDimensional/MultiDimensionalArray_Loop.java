db73e31bcb74cb1bdecff2758983957e4f298c55
Anton Vladimirov <l.a.v7232@gmail.com>
2023-03-28 19:16:11 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day21_Array_MultiDimensional/MultiDimensionalArray_Loop.java
+++ day21_Array_MultiDimensional/MultiDimensionalArray_Loop.java
@@ -7,6 +7,7 @@ public class MultiDimensionalArray_Loop {
 
         int[][] arr2D = {{10,20,30},{40,50,60,70,80},{90,100},{1000,2000}};
 
+        // regular fori nested loop to print all elements
         for (int i =0; i<arr2D.length; i++){
             int[] each1D = arr2D[i];
             System.out.println(Arrays.toString(each1D));
@@ -17,13 +18,31 @@ public class MultiDimensionalArray_Loop {
             }
         }
         System.out.println("=====================================");
-
+             // forEach nested loop to print all elements
         for(int[] each1DArray : arr2D){
             System.out.println(Arrays.toString(each1DArray));
             for(int eachElement : each1DArray) {
                 System.out.println(eachElement);
             }
         }
+        System.out.println("======================================");
+
+        // shortcut nested loop to show all elements (reverse, can be changed to normal)
+        for (int i = arr2D.length - 1; i >= 0; i--) {  // arr2D.forr
+            for (int j = arr2D[i].length - 1; j >= 0; j--) {  // arr2D[i].forr
+                System.out.println(arr2D[i][j]);
+            }
+        }
+        System.out.println("======================================");
+
+        // shortcut nested forEach loop
+        for (int[] each1DArray : arr2D) {  // arr2D.for
+            for (int eachElement : each1DArray) {  // each1DArray.for
+                System.out.println(eachElement);
+            }
+        }
+
+        }
 
     }
-}
+
