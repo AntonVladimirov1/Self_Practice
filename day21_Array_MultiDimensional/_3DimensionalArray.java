797f5d40ef62386ef9886e3e231c9c207fffb15c
Anton Vladimirov <l.a.v7232@gmail.com>
2023-03-29 17:40:22 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day21_Array_MultiDimensional/_3DimensionalArray.java
+++ day21_Array_MultiDimensional/_3DimensionalArray.java
@@ -16,5 +16,22 @@ public class _3DimensionalArray {
         System.out.println(Arrays.toString(arr3D[0][1])); // to print all elements from one inner array
         System.out.println(arr3D[0][1][2]); // to print one element from chosen inner array
 
+        System.out.println("===================================================================");
+
+        for (int i = 0; i < arr3D.length; i++) {  //
+            int[][] each2DArray = arr3D[i];
+            System.out.println(Arrays.deepToString(each2DArray));
+
+            for (int j = 0; j < each2DArray.length; j++) {
+                int[] each1DArray = each2DArray[j];
+                System.out.println(Arrays.toString(each1DArray));
+                for (int k = 0; k < each1DArray.length; k++) {
+                    int element = each1DArray[k];
+                    System.out.println(element);
+                }
+
+            }
+        }
+
     }
 }
