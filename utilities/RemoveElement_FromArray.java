59ee76ccc28a7c63438e73d7de4621ff536e8914
Anton Vladimirov <l.a.v7232@gmail.com>
2023-03-30 18:07:07 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- utilities/RemoveElement_FromArray.java
+++ utilities/RemoveElement_FromArray.java
@@ -2,17 +2,16 @@ package utilities;
 
 public class RemoveElement_FromArray {
 
-    public static int[] remove(int[] array, int index){
+    public static int[] remove(int[] array, int index) {
         int[] res = new int[array.length - 1];
         for (int i = 0, j = 0; i < array.length; i++) {
 
             if (index != i) {
                 res[j++] = array[i];
-            } else {
-
             }
         }
         return res;
+
     }
 
 
--- utilities/Runner.java
+++ utilities/Runner.java
@@ -1,9 +1,14 @@
 package utilities;
 
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
 
+        int[] numbers = {1,2,3,4,5,6,7,8};
 
+        int[] num = RemoveElement_FromArray.remove(numbers,3);
+        System.out.println(Arrays.toString(num));
 
     }
 }
