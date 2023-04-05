76176cba54c1b671150d0dcbb811c59938774ad8
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-05 13:35:48 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- Live_Sessions/Max_Second_MaxNumber.java
+++ Live_Sessions/Max_Second_MaxNumber.java
@@ -9,8 +9,8 @@ public class Max_Second_MaxNumber {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(6,599,1,9,5,2,4,25,4,584,7,8,12));
         System.out.println(list);
 
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;             //list.get(0);
+        int max = Integer.MIN_VALUE;         //* can be - list.get(0);
+        int secondMax = Integer.MIN_VALUE;   //* can be - list.get(0);
         for (Integer each : list) {
             if (each > max){
                 secondMax = max;
