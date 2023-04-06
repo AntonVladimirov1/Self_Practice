026f0681309804ba2c8e21d357f461eab3b8019c
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-05 22:57:33 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day25_Date_and_Time_Constructor/DateTimeFormatterIntro.java
+++ day25_Date_and_Time_Constructor/DateTimeFormatterIntro.java
@@ -1,11 +1,14 @@
 package day25_Date_and_Time_Constructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeFormatterIntro {
     public static void main(String[] args) {
-                           /// DateTimeFormatter
+                           /// DateFormatter
+
         DateTimeFormatter df = DateTimeFormatter.ofPattern("MM/dd/y, EEEE");
                                //* offPattern needs to be provided as a String format
         LocalDate today = LocalDate.now();
@@ -13,6 +16,16 @@ public class DateTimeFormatterIntro {
 
         LocalDate date1 = LocalDate.of(2023,8,27);
         System.out.println(date1.format(df));
+
+                           /// TimeFormatter
+        DateTimeFormatter tf = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime time1 = LocalTime.of(7,5);
+        System.out.println(time1.format(tf));
+
+                           /// Date & Time Formatter (together)
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/y (HH:mm)");
+        LocalDateTime starts = LocalDateTime.now();
+        System.out.println(starts.format(dtf));
 
     }
 }
