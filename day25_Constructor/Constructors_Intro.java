ccb08055662c87cde3f8a62328899b731023e78e
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-06 19:44:29 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day25_Constructor/Constructors_Intro.java
+++ day25_Constructor/Constructors_Intro.java
@@ -3,7 +3,10 @@ package day25_Constructor;
 public class Constructors_Intro {
 
     public Constructors_Intro(){
-
+        System.out.println("Object is created by using <No> argument constructor");
+    }
+    public Constructors_Intro(int a){
+        System.out.println("Object is created by using <INT> argument constructor");
     }
 
     public void add(){
@@ -11,8 +14,10 @@ public class Constructors_Intro {
     }
 
     public static void main(String[] args) {
-
-        new Constructors_Intro();
+                 //* Every single object must be created by using related Constructor(see above)
+        Constructors_Intro obj1 = new Constructors_Intro(10);
+        Constructors_Intro obj2 = new Constructors_Intro();
+        Constructors_Intro obj3 = new Constructors_Intro(10);
 
     }
 
