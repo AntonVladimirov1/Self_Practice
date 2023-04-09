fc55be8cc9c291b9df04ad26634b5fae7420d2f8
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-09 18:21:05 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day26_Statics/StudentTask/TestStudentObjects.java
+++ day26_Statics/StudentTask/TestStudentObjects.java
@@ -10,9 +10,28 @@ public class TestStudentObjects {
         Student student4 = new Student("Natys",24,'F',"A24A");
         Student student5 = new Student("Karry",21,'F',"A21BA");
 
+       /* ArrayList<Student> group1 =new ArrayList<>();
+        group1.addAll(Arrays.asList(student1,student2,student3,student4,student5));
+        System.out.println(group1); */
+
         StudentsGroup group1 = new StudentsGroup("All",1);
-        group1.addStudent(s);
+        group1.addStudent(student1);
+        group1.addStudent(student2);
+        group1.addStudent(student3);
+        group1.addStudent(student4);
+        group1.addStudent(student5);
         System.out.println(group1);
+        System.out.println();
+        System.out.println("=======================================================================");
+
+        group1.removeStudent("A21");
+        System.out.println(group1);
+
+        for (Student each : group1.students) {
+            System.out.println(each.name+" / "+ each.id);
+        }
+        System.out.println("========================================================================");
+
 
     }
 
