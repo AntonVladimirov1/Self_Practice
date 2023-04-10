14509c662f714105eb8589e6b0f8de67efa5f3e4
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-10 14:19:19 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day26_Statics/TestIPhone.java
+++ day26_Statics/TestIPhone.java
@@ -1,10 +1,19 @@
 package day26_Statics;
 
+import day27_AccessModifiers.Circle;
+
 public class TestIPhone {
     public static void main(String[] args) {
 
         IPhone obj1 = new IPhone("IPhone12","Black",1000);
         obj1.printPhoneInfo();
+
+        System.out.println("=========================================");
+
+        Circle first =new Circle(5);
+        System.out.println(first);
+        Circle second =new Circle(7);
+        System.out.println(second);
 
     }
 }
---
+++ day27_AccessModifiers/Circle.java
@@ -0,0 +1,29 @@
+package day27_AccessModifiers;
+
+public class Circle {
+
+    double radius;
+    double diameter;
+    public static double Pi= 3.14;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        diameter = radius*2;
+    }
+
+    public double area(){
+        return (radius*radius*Pi);
+    }
+    public double perimeter(){
+        return (diameter*Pi);
+    }
+
+    public String toString() {
+        return "\nCircle{" +
+                "radius = " + radius +
+                ", diameter = " + diameter +
+                ", Perimeter = " + perimeter() +
+                ", Area = " + area() +
+                '}';
+    }
+}
