6b607c396a5a9682801781d2c0869ba0bf2df9bf
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-12 15:12:32 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day28_OOP_Encapsulation/Getter_Setter.java
+++ day28_OOP_Encapsulation/Getter_Setter.java
@@ -2,11 +2,18 @@ package day28_OOP_Encapsulation;
 
 public class Getter_Setter {
 
+    private String name = "Masha";
     private int age = 21;
     private String style = "Slim";
     private int size = 1;
 
     //* Getter method - can ONLY READ data in different class
+    public String getName(){
+        if (name==null) {
+            System.err.println("Name has not been set");
+        }
+        return name;
+    }
     public int getAge(){
         if (age==0){
             System.err.println("Age has not been set");
@@ -23,11 +30,26 @@ public class Getter_Setter {
 
 
     //* Setter method - can change(REWRITE) private variable
+    public void setName(String name){
+        boolean hasDigits=false;
+        for (char each : name.toCharArray()) {
+            if (Character.isDigit(each)) {
+                hasDigits = true;
+                break;
+            }
+        }
+            if (hasDigits){
+                System.err.println("Invalid name - has digits");
+                System.exit(1);
+            }
+
+        this.name = name;
+    }
     public void setAge(int age){
         if (age < 16){
             System.err.println("Not allowed age less than 16< "+age);
            // return; //* exits the program
-            System.exit(1); //* allow to terminate any futher code execution
+            System.exit(1);        //* allow to terminate any futher code execution
         }
         this.age = age;
     }
--- day28_OOP_Encapsulation/Test_GetterSetter.java
+++ day28_OOP_Encapsulation/Test_GetterSetter.java
@@ -3,11 +3,15 @@ package day28_OOP_Encapsulation;
 public class Test_GetterSetter {
     public static void main(String[] args) {
 
-        Getter_Setter girl =new Getter_Setter();  //* new object
-        System.out.println(girl.getAge()+" / "+ girl.getSize()+" / "+girl.getStyle()); //* "get" Getter method
+        Getter_Setter girl1 =new Getter_Setter();  //* new object
+        System.out.println(girl1.getName()+"/"+girl1.getAge()+"/"+ girl1.getSize()+"/"+girl1.getStyle()); //* "get" Getter method
 
-        girl.setAge(15);          //* "set" Setter method
-        System.out.println(girl.getAge());
+        girl1.setAge(16);          //* "set" Setter method
+        System.out.println(girl1.getAge());
+
+        girl1.setName("Katya");
+        System.out.println(girl1.getName());
+
 
     }
 }
