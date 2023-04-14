68b0b39d25748301567b5ff16002c02570e7150e
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-14 18:15:39 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- day29_Inheritance/Encapsulation/TestStudent.java
+++ day29_Inheritance/Encapsulation/TestStudent.java
@@ -3,7 +3,7 @@ package day29_Inheritance.Encapsulation;
 public class TestStudent {
     public static void main(String[] args) {
 
-        Student student1 = new Student("Masha",17,'F','A',"MiT");
+        Student student1 = new Student("Masha",17,'F','A',"MIT");
         System.out.println(student1);
 
         student1.setAge(21);
