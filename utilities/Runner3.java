c2ae86851a0a75cc5515c34d6e440a18350c1115
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-17 16:15:19 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- utilities/Runner3.java
+++ utilities/Runner3.java
@@ -1,11 +1,7 @@
 package utilities;
 
 public class Runner3 {
-    public static void main(String[] args) {
 
-        System.out.println(Math.PI);
-        System.out.println(Math.cbrt(8));
 
-    }
 
 }
