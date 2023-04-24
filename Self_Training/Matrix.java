c905c5d1443f95c7bc0e9fbd6287db7cadbbb31f
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-24 13:08:41 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

---
+++ Self_Training/Matrix.java
@@ -0,0 +1,24 @@
+package Self_Training;
+
+public class Matrix {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {18,9,12},
+                {36,-9,91},
+                {99,33,16}
+        };
+        int target = 18;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[i][j]==target){
+                    System.out.println(i+";"+j);
+                }
+
+            }
+        }
+
+    }
+}
