97fc65707e9584ff8009b62c74b5b857cbb71863
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-25 12:21:11 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

--- Self_Training/Matrix.java
+++ Self_Training/Matrix.java
@@ -1,20 +1,31 @@
 package Self_Training;
+//            0    1    2
+//          ________________
+//        0 | 18 | 9  | 12 |
+//          ----------------
+//        1 | 36 | -9 | 3  |
+//          ----------------
+//        2 | 71 | 5  | 16 |
+//          ----------------
+// You will need to find location (in matrix) of the given number (target).
+// Rows (horizontal lines) 0 1 2 ; Columns (vertical lines) 0 1 2
+// For example - number 18: location = 0;0
 
 public class Matrix {
     public static void main(String[] args) {
 
         int[][] matrix = {
                 {18,9,12},
-                {36,-9,91},
-                {99,33,16}
+                {36,-9,3},
+                {71,5,16}
         };
-        int target = 12;
+        int target = 18;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
 
-                if (matrix[i][j]==target) {
-                    System.out.println(i + ";" + j);
+                if (matrix[row][column]==target) {
+                    System.out.println(row +";"+ column);
                 }
             }
         }
