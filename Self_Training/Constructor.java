3293c7503d28d5f7e4bb11efbefa76bee700a5b6
Anton Vladimirov <l.a.v7232@gmail.com>
2023-04-25 10:30:21 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	utilities/Runner.java

---
+++ Self_Training/Constructor.java
@@ -0,0 +1,12 @@
+package Self_Training;
+
+public class Constructor {
+
+    public Constructor() {
+        System.out.println("shit");
+    }
+
+    public static void main(String[] args) {
+        Constructor obj=new Constructor();
+    }
+}
