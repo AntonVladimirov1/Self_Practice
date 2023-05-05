50d2d4bf90326eddd0d573118c80e9c56ecd693a
Anton Vladimirov <l.a.v7232@gmail.com>
2023-05-04 19:31:10 -0500

    Interface2 added

--- day34_Abstraction_Interface/Animal_Interface/Dolphin.java
+++ day34_Abstraction_Interface/Animal_Interface/Dolphin.java
@@ -1,6 +1,6 @@
 package day34_Abstraction_Interface.Animal_Interface;
 
-public class Dolphin extends Animal implements Swimmable{
+public class Dolphin extends Animal implements Swimmable,Playable{
 
     public Dolphin(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -8,11 +8,16 @@ public class Dolphin extends Animal implements Swimmable{
 
     @Override
     public void eat() {
-        System.out.println("Dolphin "+getName()+" is eating small fish");
+        System.out.println(getName()+" is eating small fish");
     }
 
     @Override
     public void swim() {
-        System.out.println(getName()+" swimming better than other");
+        System.out.println(getName()+" swimming better than other animals");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(getName()+" is always playing with humans");
     }
 }
--- utilities/Runner3.java
+++ utilities/Runner3.java
@@ -1,13 +1,20 @@
 package utilities;
 
 
+import day34_Abstraction_Interface.Animal_Interface.Dolphin;
 import day34_Abstraction_Interface.Animal_Interface.Person;
 
 public class Runner3 {
        public static void main(String[] args) {
               Person person=new Person();
+              System.out.println(person);
               person.play();
               person.swim();
+              Dolphin dolphin=new Dolphin("Sunny","White",'M',5,"big","Grey");
+              System.out.println(dolphin);
+              dolphin.play();
+              dolphin.eat();
+              dolphin.swim();
        }
 }
 
