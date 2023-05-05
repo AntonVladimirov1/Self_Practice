ffa0ebb028276290e3a110ed8d73e6c34ec93f61
Anton Vladimirov <l.a.v7232@gmail.com>
2023-05-04 20:37:16 -0500

    Interface3 added

--- day35_Polimorphism/Transport/CydeoCar.java
+++ day35_Polimorphism/Transport/CydeoCar.java
@@ -8,37 +8,30 @@ public class CydeoCar extends Car implements Flyable,Electric,AutoPilot, Swimmab
     public CydeoCar(String make, String model, String color, int year, double price) {
         super(make, model, color, year, price);
     }
-
     @Override
     public void start() {
 
     }
-
     @Override
     public void transportPeople() {
 
     }
-
     @Override
     public void fly() {
 
     }
-
     @Override
     public void swim() {
 
     }
-
     @Override
     public void autoPark() {
 
     }
-
     @Override
     public void selfDrive() {
 
     }
-
     @Override
     public void charge() {
 
