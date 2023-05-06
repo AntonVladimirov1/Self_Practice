fd18d353f2c80cb15df90b33e8751411e3de32aa
Anton Vladimirov <l.a.v7232@gmail.com>
2023-05-06 11:04:49 -0500

    Polymorphism practice with instanceof with foreach loop

--- day35_Polymorphism/Polymorphism_Practice2.java
+++ day35_Polymorphism/Polymorphism_Practice2.java
@@ -12,7 +12,7 @@ public class Polymorphism_Practice2 {
                 new Developer("Yulia", 23, 'F',"A04", "Software Developer", 135000, "Python"),
                 new Tester("Cihad", 30, 'M',"A05", "SDET", 105000),
                 new Developer("Gulistan",  26, 'F', "A06", "Web Developer", 130000, "Python"),
-                new Tester("Nora",  28, 'F', "A07", "QE", 120000),
+                new Tester("Nora",  28, 'F', "A07", "QA", 120000),
                 new Driver("Aaron", 48, 'M', "D1", "Truck Driver", 90000),
                 new Developer("Diana",  29, 'F', "A08", "Front-end Developer", 140000, "C#"),
                 new Tester("Tatiana",  25, 'F',"A09", "SDET", 130000),
@@ -20,13 +20,13 @@ public class Polymorphism_Practice2 {
                 new Tester("Timur",  29, 'M',"A11", "SDET", 115000),
                 new Teacher("James", 45,  'M', "B1", "Math Teacher", 75000),
                 new Developer("Hasan",  26, 'F', "A12", "Full Stack Developer", 142000, "Java"),
-                new Tester("Nataliia",  24, 'F', "A13", "QE", 125000),
+                new Tester("Nataliia",  24, 'F', "A13", "QA", 125000),
                 new Developer("Ermek",  26, 'M',"A14", "Full Stack Developer", 142000, "Java"),
                 new Tester("Khashayar",  30, 'M',"A15", "SDET", 105000),
         };
            // Print the name and Job Title of every employee - foreach loop
         for (Employee each : employees){
-            System.out.println(each.getName()+" / "+ each.getJobTitle());
+            System.out.println(each.getName()+" = "+ each.getJobTitle());
         }
 
         // How many developers, testers, teachers, drivers do we have?
